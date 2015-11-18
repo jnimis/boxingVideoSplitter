21e444c4f7966c38f84e437cab58381e67d1df19
John Nimis <jnimis@yahoo.com>
2015-11-17 21:41:14 -0600

    first attempt at getting this to work, sending a command to ffmpeg extermally - I think AVFoundation will be better

--- VideoSplitter/AppDelegate.h
+++ VideoSplitter/AppDelegate.h
@@ -10,6 +10,20 @@
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
+@property (strong, nonatomic) NSString* inputPathString;
+@property (strong, nonatomic) NSString* outputPathString;
+@property (strong, nonatomic) NSString* ffmpegPath;
+@property NSInteger roundLength;
+@property NSInteger restLength;
+@property (strong) IBOutlet NSComboBox *roundDurationComboBox;
+@property (strong) IBOutlet NSComboBox *restDurationComboBox;
+@property (strong) IBOutlet NSTextField *startTimeTextField;
+
+- (IBAction)roundDurationChanged:(id)sender;
+- (IBAction)restDurationChanged:(id)sender;
+- (IBAction)chooseInputFileClicked:(id)sender;
+- (IBAction)chooseOutputFileClicked:(id)sender;
+- (IBAction)goClicked:(id)sender;
 
 @end
 
--- VideoSplitter/AppDelegate.m
+++ VideoSplitter/AppDelegate.m
@@ -17,10 +17,116 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+    self.roundLength = 180;
+    self.restLength = 60;
+    
+    // should look for ffmpeg and find it
+//    NSTask* task = [[NSTask alloc] init];
+//    [task setLaunchPath:@"/usr/bin/which ffmpeg"];
+    
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
+}
+
+- (IBAction)roundDurationChanged:(id)sender {
+    
+    NSInteger index = [sender indexOfSelectedItem];
+    switch (index) {
+        case 0:
+            self.roundLength = 180;
+            break;
+        case 1:
+            self.roundLength = 120;
+            break;
+        case 2:
+            self.roundLength = 90;
+            break;
+    }
+    
+}
+
+- (IBAction)restDurationChanged:(id)sender {
+    
+    NSInteger index = [sender indexOfSelectedItem];
+    switch (index) {
+        case 0:
+            self.restLength = 60;
+            break;
+        case 1:
+            self.restLength = 30;
+            break;
+    }
+    
+}
+
+- (IBAction)chooseInputFileClicked:(id)sender {
+    self.inputPathString = [self getPathStringFromDialogue:true];
+}
+
+- (IBAction)chooseOutputFileClicked:(id)sender {
+    self.outputPathString = [self getPathStringFromDialogue:false];
+}
+
+- (NSString*)getPathStringFromDialogue:(bool)willSelectFile {
+    
+    __block NSString* pathString;
+    
+    NSOpenPanel* panel = [NSOpenPanel openPanel];
+    
+    if (willSelectFile) {
+        [panel setCanChooseFiles:YES];
+    } else {
+        [panel setCanChooseDirectories:YES];
+    }
+    [panel setAllowsMultipleSelection:false];
+    
+    if ( [panel runModal] == NSOKButton) {
+        NSArray* files = [panel URLs];
+        pathString = [NSString stringWithFormat:@"%@",[files objectAtIndex:0]];
+    }
+    
+    return pathString;
+}
+
+- (IBAction)goClicked:(id)sender {
+    bool shouldContinue = true;
+    
+    int startTime = (int)([self.startTimeTextField integerValue] - 1);
+    if (startTime < 0) {
+        startTime = 0;
+    }
+    int increment = (int)(self.roundLength + self.restLength);
+    int roundInt = (int)(self.roundLength + 2);
+    int index = 1;
+    
+    // check to make sure controls have valid values (esp. input path, output path, start time)
+    if ((self.inputPathString != nil) && [self.outputPathString hasSuffix:@"/"] && (startTime > 0)) {
+            
+        // parse paths
+        NSString* outputPath = [self.outputPathString substringFromIndex:7];
+        NSString* inputPath = [self.inputPathString substringFromIndex:7];
+        
+        // determine overall file length to define path loop length
+        while (shouldContinue) {
+            
+            NSString* outFilePath = [NSString stringWithFormat:@"%@outputFile%d.mp4",outputPath,index];
+            NSTask *task = [[NSTask alloc] init];
+            [task setLaunchPath:@"/usr/bin/ffmpeg"];
+            NSArray* arguments = [NSArray arrayWithObjects: @"-i", inputPath, @"-ss", [NSString stringWithFormat:@"%d",startTime], @"-t",
+                                  [NSString stringWithFormat:@"%d",roundInt], outFilePath, nil];
+            [task setArguments:arguments];
+            [task launch];
+            [task waitUntilExit];
+            
+            startTime = startTime + increment;
+            index++;
+            if (index > 5) {
+                shouldContinue = false;
+            }
+        }
+    }
 }
 
 @end
